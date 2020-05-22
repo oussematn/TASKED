4e0f52ebd6ed365f165774586b76da7fb396fe30
oussema insat <oussema.miled@insat.u-carthage.tn>
2020-05-22 12:31:40 +0100

    Customizing guest page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -78,7 +78,6 @@
                 </div>
             </div>
         </nav>
-
         <main class="py-4">
             @yield('content')
         </main>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,9 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
-
+    <title>TASKED</title>
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Fonts -->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
@@ -20,6 +21,7 @@
             font-weight: 200;
             height: 100vh;
             margin: 0;
+            overflow-x: hidden;
         }
 
         .full-height {
@@ -63,11 +65,42 @@
         .m-b-md {
             margin-bottom: 30px;
         }
+
+        .content {
+            margin-top: 23vh;
+            display: grid;
+            grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+            grid-gap: 5vw;
+            margin-left: 9vw;
+        }
+
+        .welcome {
+            margin-top: 5vh;
+        }
+
+        .blabla {
+            font-size: 18px
+        }
+
+        .image img {
+            margin-top: -10vh;
+            height: 60vh;
+        }
+
+        .display-1 {
+            color: #7DB5DB;
+        }
+
+        @media only screen and (max-width :1000px) {
+            .content {
+                margin-left: 0;
+            }
+        }
     </style>
 </head>
 
 <body>
-    <div class="flex-center position-ref full-height">
+    <div class="flex-center position-ref">
         @if (Route::has('login'))
         <div class="top-right links">
             @auth
@@ -81,6 +114,21 @@
             @endauth
         </div>
         @endif
+    </div>
+    <div class="container content">
+        <div class="welcome">
+            <h1 class="display-1">
+                Tasked
+            </h1>
+            <div class="blabla">
+                <span>Keep yourself organized!</span>
+                <br>
+                <span>Manage Less. Do More.</span>
+            </div>
+        </div>
+        <div class="image">
+            <img src="{{ asset('images/illust.png') }}" alt="Tasked">
+        </div>
     </div>
 </body>
 
